eeece2f88e26b94fc6cf0ca3b4e61a3446491879
Aaron Lun <alun@wehi.edu.au>
2016-11-17 15:45:33 +0000

    Prettified UI.

--- R/interpretSpheres.R
+++ R/interpretSpheres.R
@@ -1,4 +1,4 @@
-interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.height=100, xlim=NULL, p=0.01, run=TRUE, ...)   
+interpretSpheres <- function(x, markers=NULL, labels=NULL, metrics=NULL, num.per.row=6, plot.height=100, xlim=NULL, p=0.01, run=TRUE, ...)   
 # This creates a Shiny app to assist interpretation of the hyperspheres.
 #
 # written by Aaron Lun
@@ -11,11 +11,7 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
     collim <- intensityRanges(x, p=p)
 
     coords <- as.matrix(coords)
-    if (is.null(rownames(coords))) {
-        sphere.names <- seq_len(nrow(coords))
-    } else {
-        sphere.names <- rownames(coords)
-    }
+    N <- nrow(coords)
 
     # Checking marker order.
     if (!is.null(markers)) { 
@@ -27,10 +23,15 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
 
     # Using existing labels if provided.
     if (is.null(labels)) {
-        labels <- character(length(sphere.names))
+        labels <- character(N)
     } else {
-        stopifnot(length(labels)==length(sphere.names))
+        stopifnot(length(labels)==N)
     } 
+
+    # Checking metrics if provided.
+    if (!is.null(metrics)) {
+        stopifnot(nrow(metrics)==N)
+    }
 
     # Setting up the internal plotting and storage.
     collected <- new.env()
@@ -44,10 +45,18 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
     ui <- pageWithSidebar(
                     headerPanel("Interpreting hypersphere coordinates"),
                     sidebarPanel(
+                                 h4("Metrics"),
+                                 tableOutput("metrics"),
+                                 hr(size=30),
                                  h4("History:"),
-                                 tableOutput("history"),
+                                 tableOutput("history"), 
+                                 hr(size=30),
                                  h4("Closest labelled:"),
-                                 tableOutput("closest")
+                                 tableOutput("closest"), 
+                                 hr(size=30),
+                                 textInput("extraplot", "Add more hyperspheres:", value=""),
+                                 actionButton("addtoplot", "Add to plot"),
+                                 actionButton("clearplot", "Clear")
                                  ),
                     mainPanel(
                               plotOutput("histograms", height = plot.height), 
@@ -74,11 +83,37 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
                      )
 
     # Setting up updating functions.
-    updatePlot <- function(output) {
+    updatePlot <- function(output, extras=NULL) {
         output$histograms <- renderPlot({
             par(mfrow=c(nrows, num.per.row), mar=c(2.1, 1.1, 2.1, 1.1))
-            .generateDensity(coords[collected$current,], all.dens, xlim=xlim, collim=collim, ordering=m.order) 
+            .generateDensity(coords, all.dens, xlim=xlim, collim=collim, ordering=m.order,
+                             current=collected$current, extras=extras) 
         })
+    }
+
+    updateMetrics <- function(output) {
+        output$metrics <- renderTable({
+            out.metrics <- c("Number", "Label")
+            out.vals <- c(collected$current, collected$labels[collected$current])
+
+            if (!is.null(metrics)) { 
+                extra.metrics <- colnames(metrics)
+                extra.vals <- list() 
+                for (met in seq_along(extra.metrics)) {
+                    my.val <- metrics[collected$current,met]
+                    if (is.double(my.val)) {
+                        my.val <- format(my.val, digits=5)
+                    } else {
+                        my.val <- as.character(my.val)
+                    }
+                    extra.vals[[met]] <- my.val
+                }
+                out.metrics <- c(out.metrics, extra.metrics)
+                out.vals <- c(out.vals, unlist(extra.vals))
+            }
+
+            data.frame(paste0("<b>", out.metrics, "</b>"), out.vals)
+        }, colnames=FALSE, sanitize.text.function=function(x) {x})
     }
 
     updateHistory <- function(output, advance=TRUE) {
@@ -87,7 +122,6 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
                 collected$history <- c(collected$current, collected$history)[1:5]
             }
             data.frame(Number=as.character(collected$history),
-                Name=sphere.names[collected$history],
                 Label=collected$labels[collected$history])
         })
     }
@@ -102,49 +136,73 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
             }
             o <- order(all.dist)[1:5]
             closest <- is.anno[o]
-            data.frame(Distance=all.dist[o], Number=as.character(closest), 
-                Name=sphere.names[closest], Label=collected$labels[closest])
+            data.frame(Distance=all.dist[o], Number=as.character(closest), Label=collected$labels[closest])
         })
-
     }
 
     # Setting up the server actions.
     server <- function(input, output) {
         updatePlot(output)
+        updateMetrics(output)
         updateHistory(output)
         updateClosest(output)
 
         observeEvent(input$previous, {
             if (collected$current!=1L) { 
                 collected$current <- collected$current - 1L
+            } else {
+                warning("index cannot be negative")
             }
             updatePlot(output)
+            updateMetrics(output)
             updateHistory(output)
             updateClosest(output)
         })
 
         observeEvent(input$continue, {
-            if (collected$current!=length(sphere.names)) {
+            if (collected$current!=N) {
                 collected$current <- collected$current + 1L
+            } else {
+                warning("index greater than number of hyperspheres")
             }
             updatePlot(output)
+            updateMetrics(output)
             updateHistory(output)  
             updateClosest(output)  
+        })
+
+        observeEvent(input$go, {
+            candidate <- as.integer(input$gotonum)
+            if (is.finite(candidate) && candidate >= 1L && candidate <= N) {
+                collected$current <- candidate
+            } else {
+                warning("index out of range of number of hyperspheres")
+            }
+            updatePlot(output)
+            updateMetrics(output)
+            updateHistory(output)
+            updateClosest(output)
+        })
+
+        observeEvent(input$addtoplot, {
+            extras <- as.integer(unlist(strsplit(input$extraplot, " ") ))
+            extras <- extras[!is.na(extras)]
+            invalid <- !is.finite(extras) && extras < 1L && extras > N
+            if (any(invalid)) {
+                warning("indices out of range of number of hyperspheres")
+                extras <- extras[!invalid]
+            }
+            updatePlot(output, extras=extras)
+        })
+
+        observeEvent(input$clearplot, {
+            updatePlot(output)
         })
 
         observeEvent(input$addlabel, {
             collected$labels[collected$current] <- input$label
             updateHistory(output, advance=FALSE)
-        })
-
-        observeEvent(input$go, {
-            candidate <- as.integer(input$gotonum)
-            if (is.finite(candidate) && candidate >= 1L && candidate <= length(sphere.names)) {
-                collected$current <- candidate
-                updatePlot(output)
-                updateHistory(output)
-                updateClosest(output)
-            }
+            updateMetrics(output)
         })
 
         observeEvent(input$finish, {
@@ -176,9 +234,10 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
     return(collected)
 }
 
-.generateDensity <- function(current.coords, density.data, ordering, collim, xlim, ...) 
+.generateDensity <- function(coords, density.data, ordering, collim, xlim, current, extras, ...) 
 # Plotting the densities and adding the point corresponding to the current coordinates.
 {
+    current.coords <- coords[current,]
     all.markers <- names(current.coords)
     all.cols <- viridis(256)
 
@@ -197,7 +256,21 @@ interpretSpheres <- function(x, markers=NULL, labels=NULL, num.per.row=6, plot.h
     
         curpos <- current.coords[m]
         curpos <- pmin(xlim[2], pmax(xlim[1], curpos))
-        points(curpos, approx(my.x, my.y, curpos, rule=2)$y, pch=16, col="red", cex=1.5)
+        cury <- approx(my.x, my.y, curpos, rule=2)$y
+        par(xpd=TRUE)
+        points(curpos, cury, pch=16, col="red", cex=1.5)
+
+        if (length(extras)) { 
+            text(curpos, cury, pos=3, current, col="red")
+            for (ex in extras) {
+                expos <- coords[ex,m]
+                expos <- pmin(xlim[2], pmax(xlim[1], expos))
+                exy <- approx(my.x, my.y, expos, rule=2)$y
+                points(expos, exy, pch=16, col="red", cex=1.5)
+                text(expos, exy, pos=3, ex, col="red")
+            }
+        }
+        par(xpd=FALSE)
     }
 
     return(invisible())
